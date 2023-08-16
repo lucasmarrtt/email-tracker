0e7703b89e848305c8a2d3150ba677d27f6dda09
lucasmarrtt <lucasschnneider009@gmail.com>
2023-08-15 21:41:32 -0300

    Definindo bot

---
+++ home/static/assets/js/script.js
@@ -0,0 +1,4 @@
+
+/* Show Chat Bot  */
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot")); 
