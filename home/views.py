ab11a238aa4cbbf210eff6bc1f45a63a99258f9c
lucasmarrtt <lucasschnneider009@gmail.com>
2024-02-16 02:01:09 -0300

    Modificando o design

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 import requests
 from . models import Search
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -13,33 +14,7 @@ import json
 
 HUNTER_API_KEY = 'f682560568da2d1e777425be0d561920ba8ba909'
 
-'''
-def index(request):
-    if request.method == 'POST':
-        search = request.POST.get('search')
 
-        if not search:
-            return HttpResponse("Nenhum termo de pesquisa fornecido.")
-
-        api_result = requests.get(f'https://api.hunter.io/v2/domain-search?domain={search}&api_key={HUNTER_API_KEY}')
-
-        if api_result.status_code == 200:
-            response_data = api_result.json()
-
-            # Exibindo o JSON retornado pela API no console do Django
-            print(json.dumps(response_data, indent=2))
-
-            # Restante do código para processar os dados e renderizar o template
-            # ...
-
-            return HttpResponse("Consulta bem-sucedida! Verifique o console para visualizar o JSON retornado pela API.")
-        else:
-            print(f"Erro da API: {api_result.status_code}")
-            return HttpResponse("Erro da API: Não foi possível obter os dados.")
-    else:
-        return render(request, 'index.html')
-
-'''
 
 def index(request):
     search_confirm = False 
@@ -50,11 +25,6 @@ def index(request):
             return HttpResponse("Nenhum termo de pesquisa fornecido.")
 
         api_result = requests.get(f'https://api.hunter.io/v2/domain-search?domain={search}&api_key={HUNTER_API_KEY}')
-
-        get_search = Search()
-        get_search.term = search
-        get_search.save()
-
 
         if api_result.status_code == 200:
             response_data = api_result.json()
@@ -91,12 +61,21 @@ def index(request):
 
             }
 
+
+            return JsonResponse(context)
+        else:
+            return JsonResponse({'error': f"Erro da API: {api_result.status_code}"})
+
+        '''
             return render(request, 'new_index.html', context)
         else:
             print(f"Erro da API: {api_result.status_code}")
             return HttpResponse("Erro da API: Não foi possível obter os dados.")
+        '''
+
     else:
-        return render(request, 'new_index.html')
+        return render(request, 'home.html')
+
 
 
 def sign_out(request):
